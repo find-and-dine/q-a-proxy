b929f06315589e221612dbdc9a03efacc726d2d1
Jeremy Sim <jsim0809@gmail.com>
2020-03-25 15:10:18 -0700

    Updating index.html.

--- public/index.js
+++ public/index.js
@@ -1,14 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-
 document.querySelector("#photos").innerHTML = 'Foo';
 document.querySelector("#menu").innerHTML = 'Bar';
 document.querySelector("#reviews").innerHTML = 'Baz';
-
-// fetch("http://localhost:3004/47")
-//   .then(response => {
-//     return response.text()
-//   })
-//   .then(data => {
-//     document.querySelector("#q-a").innerHTML = data;
-//   });
