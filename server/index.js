f3edf8d2c099fe0196ac1a8b1ba72d9e61f39f74
Jeremy Sim <jsim0809@gmail.com>
2020-03-27 15:09:19 -0700

    Now actually proxies for the Q&A element

--- server/index.js
+++ server/index.js
@@ -7,6 +7,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/:restaurantID', express.static(path.resolve(__dirname, '..', 'public')));
 
+app.get('/api/questions/:restaurantID', (req, res) => {
+  const { restaurantID } = req.params;
+  fetch(`http://54.70.189.204:3004/api/questions/${restaurantID}/`)
+      .then((data) => data.json())
+      .then((questions) => {
+        res.status(200).send(questions);
+      })
+      .catch(() => {
+        res.sendStatus(500);
+      });
+});
+
 app.listen(3000, () => {
   console.log('Proxy server listening on port 3000.');
 })
